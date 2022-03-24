2afb370555243d80c35c72c3b76edf071e697b90
gowtham2666 <gowtham2666@gmail.com>
2022-03-24 14:58:29 +0530

    new commit

---
+++ Quizprogram.java
@@ -0,0 +1,24 @@
+package mphasis.basis;
+import java .util.Scanner;
+
+public class Quizprogram {
+	public static void main(String[] args) {
+	int n;
+	Scanner sc = new Scanner(System.in);
+ System.out.println("who is the hero of RRR" );
+ System .out.println (" 1. gowtham \n 2.gunti \n 3.gowtham \n 4. NTR");
+ System .out.println( " enter the option: ");
+ n =sc.nextInt();
+ if (n==4) {
+	 System.out.println ("aswer is correct");
+	  
+ }
+ else {
+	 System.out.println(" wrong aswer") ;
+	  break;
+ }
+ 
+}
+	
+	
+}
